c5c3ddacd3511cca86894e831b74b50164ec8f20
Mrjorjar <mrjorjar06@gmail.com>
2025-11-07 20:04:08 +0100

    Update BouncePad.cs

--- Rab_proyec_edu/Assets/RAB_Root/Scripts/BouncePad.cs
+++ Rab_proyec_edu/Assets/RAB_Root/Scripts/BouncePad.cs
@@ -9,6 +9,7 @@ public class BouncePad : MonoBehaviour
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     public float bounceForce = 20f;          
     public string playerTag = "Player";     
     public GameObject bounceEffect;          
@@ -23,6 +24,8 @@ public class BouncePad : MonoBehaviour
 >>>>>>> a8ba3f5bb9fe1b2325630dafe6fa158633f87c53
 =======
 >>>>>>> a8ba3f5bb9fe1b2325630dafe6fa158633f87c53
+=======
+>>>>>>> e8e6c1a2e7df372e88884dd0c13e8ecbb41dea3b
     public float bounceForce = 10f;          // Fuerza del salto
     public string playerTag = "Player";      // Tag del jugador
     public GameObject bounceEffect;          // Efecto visual opcional
